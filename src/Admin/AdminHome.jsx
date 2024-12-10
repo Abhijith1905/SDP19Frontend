44dd871afd8c6c3fd19f38122fd7c0fe41582a14
Abhijith1905 <143542368+Abhijith1905@users.noreply.github.com>
2024-12-10 21:29:15 +0530

    Update AdminHome.jsx

--- src/Admin/AdminHome.jsx
+++ src/Admin/AdminHome.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import config from "../config";
 
 export default function AdminHome() {
   const [counts, setCounts] = useState({
