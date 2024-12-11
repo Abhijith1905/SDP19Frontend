e7ccdd7dcb3708a6f57c7efaab91dcfa3d38f2da
Abhijith1905 <143542368+Abhijith1905@users.noreply.github.com>
2024-12-11 21:05:03 +0530

    Update ViewProject.jsx

--- src/Student/ViewProject.jsx
+++ src/Student/ViewProject.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import config from "../config";
 
 const ViewProject = () => {
   const { id } = useParams();
