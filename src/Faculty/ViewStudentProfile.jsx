e4dcda68ca89b5a483be175f2086a56aea33e8cb
Abhijith1905 <143542368+Abhijith1905@users.noreply.github.com>
2024-12-24 13:39:04 +0530

    Update ViewStudentProfile.jsx

--- src/Faculty/ViewStudentProfile.jsx
+++ src/Faculty/ViewStudentProfile.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { User, Save, AlertCircle } from 'lucide-react';
+import config from '../config';
 
 export default function ViewStudentProfile() {
   const [studentData, setStudentData] = useState({});
