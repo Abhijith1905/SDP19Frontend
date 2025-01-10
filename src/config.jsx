c03b9d78506522ddeba4d3111308a81276697b30
Abhijith1905 <143542368+Abhijith1905@users.noreply.github.com>
2025-01-10 22:58:10 +0530

    Update config.jsx

--- src/config.jsx
+++ src/config.jsx
@@ -1,7 +1,8 @@
 const config =
 {
 // url:'http://localhost:2025'
-url:'https://edusupport-backend.up.railway.app'
+// url:'https://edusupport-backend.up.railway.app'
+  url: 'http://ec2-204-236-199-164.compute-1.amazonaws.com:8080/SDPProject-0.0.1-SNAPSHOT'
 
 };
 export default config;
