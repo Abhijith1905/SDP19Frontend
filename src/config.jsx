28c3a94d55af51613a637d9fff73f05cac9369fd
Abhijith1905 <143542368+Abhijith1905@users.noreply.github.com>
2025-02-08 22:40:34 +0530

    Update config.jsx

--- src/config.jsx
+++ src/config.jsx
@@ -1,8 +1,8 @@
 const config =
 {
 // url:'http://localhost:2025'
-// url:'https://edusupport-backend.up.railway.app'
-  url: 'http://ec2-204-236-199-164.compute-1.amazonaws.com:8080/SDPProject-0.0.1-SNAPSHOT'
+   url:'https://p2msbackend-ergva2eyh2c5f9fk.canadacentral-01.azurewebsites.net'
+ // url: 'http://ec2-204-236-199-164.compute-1.amazonaws.com:8080/SDPProject-0.0.1-SNAPSHOT'
 
 };
 export default config;
